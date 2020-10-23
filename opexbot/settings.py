725b65b5bfe77268d10b838a10e6fbbedb2d831f
Elmir <sufi4483@gmail.com>
2020-10-23 12:21:20 +0400

    Postgre database for production

--- opexbot/settings.py
+++ opexbot/settings.py
@@ -85,22 +85,40 @@ WSGI_APPLICATION = 'opexbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+if DEBUG == False:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'opexbot',
+            'NAME': 'opexbotDB',
 
-        'USER': 'postgres',
+            'USER': 'opexbot_admin',
 
-        'PASSWORD': 'sufi4483',
+            'PASSWORD': 'testing123',
 
-        'HOST': 'localhost',
+            'HOST': 'localhost',
 
-        'PORT': '5432',
+            'PORT': '5432',
 
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+            'NAME': 'opexbot',
+
+            'USER': 'postgres',
+
+            'PASSWORD': 'sufi4483',
+
+            'HOST': 'localhost',
+
+            'PORT': '5432',
+
+        }
+    }
 
 
 # Password validation
