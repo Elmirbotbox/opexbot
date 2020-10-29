0bc8a6d4e30a3da4427002ce2aabf8db33ec9cd3
Elmirbotbox <70257750+Elmirbotbox@users.noreply.github.com>
2020-10-29 11:17:08 +0400

    Update settings.py

--- opexbot/settings.py
+++ opexbot/settings.py
@@ -162,17 +162,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-JWT_AUTH = {
-    # how long the original token is valid for
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
 
-    # allow refreshing of tokens
-    'JWT_ALLOW_REFRESH': True,
-
-    # this is the maximum time AFTER the token was issued that
-    # it can be refreshed.  exprired tokens can't be refreshed.
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=1),
-}
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
